74360cbcf26e8aa259cb8898c38538f09a7d4909
Quanlong <kyan.ql.he@gmail.com>
2015-12-02 11:30:54 +0800

    Update formula to reference v0.0.2

--- launchd-oneshot.rb
+++ launchd-oneshot.rb
@@ -2,8 +2,8 @@ class LaunchdOneshot < Formula
   desc "Add a oneshot launchd jobs"
   homepage "https://github.com/cybertk/launchd-oneshot"
   url "https://github.com/cybertk/launchd-oneshot.git",
-    :tag => "v0.0.1",
-    :revision => "c905bb15f59d19fb858569f6b0f9fa77e4201e19"
+    :tag => "v0.0.2",
+    :revision => "0ff1208011f0878a5af16b29fd3a3291fcebcad9"
 
   head "https://github.com/cybertk/launchd-oneshot.git"
 
