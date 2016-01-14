eb03559316216fe1fd8a915c0cfb92ab98646445
Quanlong <kyan.ql.he@gmail.com>
2016-01-14 16:41:34 +0800

    Test agents keys

--- tests/cli-test.sh
+++ tests/cli-test.sh
@@ -3,8 +3,10 @@
 fixtures() {
     PATH="$BATS_TEST_DIRNAME/..:$PATH"
     FIXTURE_DIR="$BATS_TEST_DIRNAME/fixtures"
+    SCRIPT_PATH="$(cd "$BATS_TEST_DIRNAME/.." && pwd)/launchd-oneshot"
 
     JOBS_DIR=/usr/local/var/launchd-oneshot/jobs
+    LOGS_DIR=/usr/local/var/log/launchd-oneshot
 }
 
 setup() {
@@ -12,15 +14,14 @@ setup() {
 }
 
 teardown() {
-    launchctl unload /Library/LaunchAgents/com.cybertk.launchd-oneshot.job.sh.trigger.plist
-    sudo launchctl unload /Library/LaunchAgents/com.cybertk.launchd-oneshot.job.sh.trigger.plist
-    sudo launchctl unload /Library/LaunchDaemons/com.cybertk.launchd-oneshot.job.sh.plist
+    launchctl unload /Library/LaunchAgents/com.cybertk.launchd-oneshot.*.plist
+    sudo launchctl unload /Library/LaunchAgents/com.cybertk.launchd-oneshot.*.plist
 
-    sudo rm /Library/LaunchAgents/com.cybertk.launchd-oneshot.job.sh.trigger.plist
-    sudo rm /Library/LaunchDaemons/com.cybertk.launchd-oneshot.job.sh.plist
+    sudo rm /Library/LaunchAgents/com.cybertk.launchd-oneshot.*.plist
+    sudo rm /Library/LaunchDaemons/com.cybertk.launchd-oneshot.*.plist
 
-    sudo rm -f /tmp/launchd-oneshot.test
-    sudo rm -f /usr/local/var/log/launchd-oneshot/job.sh.log
+    sudo rm -f /tmp/launchd-oneshot*
+    sudo rm -f /usr/local/var/log/launchd-oneshot/*
 
     sleep 1
 }
@@ -34,6 +35,7 @@ fixtures
 
 @test "installing a valid job" {
     expected_job=valid.job
+    expected_agent=/Library/LaunchDaemons/com.cybertk.launchd-oneshot.$expected_job.plist
 
     run sudo launchd-oneshot "$FIXTURE_DIR/$expected_job"
     # it should exit 0
@@ -41,12 +43,23 @@ fixtures
     # it should installed job under $JOBS_DIR
     [ -f "$JOBS_DIR/$expected_job" ]
     # it should installed agent under /Library/LaunchDaemons/
-    [ -f /Library/LaunchDaemons/com.cybertk.launchd-oneshot.$expected_job.plist ]
-    plutil -lint /Library/LaunchDaemons/com.cybertk.launchd-oneshot.$expected_job.plist
+    [ -f "$expected_agent" ]
+    plutil -lint "$expected_agent"
+    # it should have correct key/values in agent
+    defaults read "$expected_agent" Label | grep "^com.cybertk.launchd-oneshot.$expected_job$"
+    defaults read "$expected_agent" Program | grep "^$SCRIPT_PATH$"
+    defaults read "$expected_agent" ProgramArguments | wc -l | grep "4"
+    defaults read "$expected_agent" ProgramArguments | grep "$SCRIPT_PATH"
+    defaults read "$expected_agent" ProgramArguments | grep "$expected_job"
+    defaults read "$expected_agent" EnvironmentVariables | grep '"LAUNCHD_ONESHOT_JOB" = 1;'
+    defaults read "$expected_agent" KeepAlive | grep "SuccessfulExit = 0;"
+    defaults read "$expected_agent" RunAtLoad | grep "^1$"
+    defaults read "$expected_agent" StandardErrorPath | grep "^$LOGS_DIR/$expected_job.log$"
+    defaults read "$expected_agent" StandardOutPath | grep "^$LOGS_DIR/$expected_job.log$"
 }
 
 
-@test "when job is complete" {
+@test "load a valid --on-boot job" {
     expected_job=valid.job
     expected_job_signature=/tmp/launchd-oneshot-fixtures.valid.job.signature
     run sudo launchd-oneshot "$FIXTURE_DIR/$expected_job"
@@ -67,6 +80,8 @@ fixtures
 
 @test "installing a valid job with --on-login" {
     expected_job=valid.job
+    expected_agent=/Library/LaunchDaemons/com.cybertk.launchd-oneshot.$expected_job.plist
+    expected_trigger_agent=/Library/LaunchAgents/com.cybertk.launchd-oneshot.$expected_job.trigger.plist
 
     run sudo launchd-oneshot "$FIXTURE_DIR/$expected_job" --on-login
     # it should exit 0
@@ -74,9 +89,32 @@ fixtures
     # it should installed job under $JOBS_DIR
     [ -f "$JOBS_DIR/$expected_job" ]
     # it should installed agent under /Library/LaunchDaemons/
-    [ -f /Library/LaunchDaemons/com.cybertk.launchd-oneshot.$expected_job.plist ]
-    plutil -lint /Library/LaunchDaemons/com.cybertk.launchd-oneshot.$expected_job.plist
+    [ -f "$expected_agent" ]
+    plutil -lint "$expected_agent"
+    # it should have correct key/values in agent
+    defaults read "$expected_agent" Label | grep "^com.cybertk.launchd-oneshot.$expected_job$"
+    defaults read "$expected_agent" Program | grep "^$SCRIPT_PATH$"
+    defaults read "$expected_agent" ProgramArguments | wc -l | grep "4"
+    defaults read "$expected_agent" ProgramArguments | grep "$SCRIPT_PATH"
+    defaults read "$expected_agent" ProgramArguments | grep "$expected_job"
+    defaults read "$expected_agent" WatchPaths | grep "/tmp/com.cybertk.launchd-oneshot.$expected_job.option"
+    defaults read "$expected_agent" EnvironmentVariables | grep '"LAUNCHD_ONESHOT_JOB" = 1;'
+    defaults read "$expected_agent" KeepAlive | grep "SuccessfulExit = 0;"
+    defaults read "$expected_agent" RunAtLoad | grep "^1$"
+    defaults read "$expected_agent" StandardErrorPath | grep "^$LOGS_DIR/$expected_job.log$"
+    defaults read "$expected_agent" StandardOutPath | grep "^$LOGS_DIR/$expected_job.log$"
     # it should installed corresponding trigger agent under /Library/LaunchAgents/
-    [ -f /Library/LaunchAgents/com.cybertk.launchd-oneshot.$expected_job.trigger.plist ]
-    plutil -lint /Library/LaunchAgents/com.cybertk.launchd-oneshot.$expected_job.trigger.plist
+    [ -f "$expected_trigger_agent" ]
+    plutil -lint "$expected_trigger_agent"
+    # it should have correct key/values in trigger agent
+    defaults read "$expected_trigger_agent" Label | grep "^com.cybertk.launchd-oneshot.$expected_job.trigger$"
+    defaults read "$expected_trigger_agent" Program | grep "^$SCRIPT_PATH$"
+    defaults read "$expected_trigger_agent" ProgramArguments | wc -l | grep "4"
+    defaults read "$expected_trigger_agent" ProgramArguments | grep "$SCRIPT_PATH"
+    defaults read "$expected_trigger_agent" ProgramArguments | grep "$expected_job.trigger"
+    defaults read "$expected_trigger_agent" EnvironmentVariables | grep '"LAUNCHD_ONESHOT_JOB" = 1;'
+    defaults read "$expected_trigger_agent" KeepAlive | grep "SuccessfulExit = 0;"
+    defaults read "$expected_trigger_agent" RunAtLoad | grep "^1$"
+    defaults read "$expected_trigger_agent" StandardErrorPath | grep "^$LOGS_DIR/$expected_job.log$"
+    defaults read "$expected_trigger_agent" StandardOutPath | grep "^$LOGS_DIR/$expected_job.log$"
 }
